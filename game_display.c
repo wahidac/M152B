5caf4118481d092be5a1cf02e4bff8fa04d314a6
Wahid <wahidac@gmail.com>
2012-03-07 15:58:47 -0800

    added functionality to randomly draw and erase smileys in grid boxes. Still need a way to get a random seed so that each run of the program is not the same

--- game_display.c
+++ game_display.c
@@ -18,6 +18,9 @@
 // Located in: ppc405_0/include/xparameters.h
 #include "xparameters.h"
 #include "xutil.h"
+#include <stdlib.h>
+#include <time.h>
+
 
 #define HORIZONTAL_PIXELS 640
 #define VERTICAL_PIXELS 480
@@ -38,8 +41,11 @@ enum _COLOR {
 // funtion prototypes
 int runTestPatternRGB(int  word_count);
 void colorPixel(COLOR c, int x, int y, unsigned int **pDisplay_data, int *x0, int *y0);
-void drawSmiley(int x0, int y0, int radius, int is_alive);
+void drawSmiley(int x0, int y0, int radius, int is_alive, int is_removing_a_smiley);
 int drawDigit(int x, int y, char digit, int scale_factor, COLOR c);
+void drawGrid(COLOR c);
+int drawRandomSmileys(int numSmileys, int smileyPos[9]);
+void gameLoop();
 
 
 //Helper functions for drawSmiley
@@ -56,15 +62,309 @@ int main (void) {
 
 	//while(1){
 		print("dbg\r\n");
-		//runTestPatternRGB(309760);
-		//drawSmiley(430,350,65,1);
-		//drawSmiley(430,100,65,0);
-	}
+		runTestPatternRGB(309760);
+		drawGrid(green);
+		gameLoop();
+		//drawSmiley(430,350,65,1,0);
+		//drawSmiley(430,100,65,0,0);
+	//}
   
 
    print("-- Exiting main() --\r\n");
    return 0;
 }
+
+
+
+/* Main game loop. Could copy into main */
+
+void gameLoop() {
+    int smileyPos[9];
+   // time_t timeOnGrid[9]; //How long have smileys been either dead or alive?
+    int i = 0;
+    int user_score = 0;  //For testing to make sure as score increases, so does randomness + speed
+    int num_smileys_allowed = 9; //Max number of smileys to have out at any given moment (cap at 6 maybe)
+    int num_smileys_on_screen = 0;
+    int max_num_smileys = 6;
+    //variable for time to keep dead smiley on screen
+    int remove_face = 500000; 
+    int add_face = 500000;
+    int random_number = 0;
+
+    //initialize grid to empty
+    for(i = 0; i < 9; i++) {
+	    smileyPos[i] = 0;
+	}
+	
+    while(1) {
+	
+ 
+        //will probably need to tweak setting of remove_face and add_face
+        //remove_face-- everytime we hit a face, and add_face for every 5 faces maybe?
+        //increase max num moles after a shit load of them have been hit
+      
+
+        if(num_smileys_on_screen > 0) {
+           /* Generate discrete random number from range 1 to remove_face.
+		   Only if random number is 1 should you remove a random smiley face.
+		   The higher remove_face is in value, the smaller the possibility
+		   that the random number generated will equal 1. */
+			
+		    random_number = rand() % remove_face;
+            if(random_number == 1) {
+                removeRandomSmiley(smileyPos);
+                num_smileys_on_screen--;
+            }
+        }
+
+        if(num_smileys_on_screen < num_smileys_allowed) {
+           /* Generate discrete random number from range 1 to add_face.
+		   Only if random number is 1 should you add a random smiley face.
+		   The higher add_face is in value, the smaller the possibility
+		   that the random number generated will equal 1. */
+			random_number = rand() % add_face;
+            if(random_number == 1) {
+                drawRandomSmileys(1,smileyPos); 
+                num_smileys_on_screen++;
+            }
+        } 
+
+        //For testing, decrease probabilities steadly here
+   
+
+        //check user grid position and see if he/she hit anything. 
+		//also, update position of gloves. this might be nasty because you need to
+		//redraw anything the glove previously covered up. Not sure what the best
+		//way to approach that is
+		//if so, mark mole dead and take note
+        //of when it died. Remove it maybe .3 seconds later or so (can decrease as the game speeds up so
+        //make proportional to remove_face or add_face? maybe keep an array of times to check on this?
+		
+		}
+            
+}
+
+
+
+
+/* Remove a random smiley. Return 1 if a smiley
+   was successfully removed, 0 else
+*/
+
+int removeRandomSmiley(int smileyPos[9]) {
+    int i = 0;
+    int availableBoxes[9];
+    int x = 0;
+    int y = 0;
+    int size = 0;
+	 //  unsigned int seed = 0;  // = clock();
+	//time_t seconds;
+	//time(&seconds);
+	//unsigned int seed = (unsigned int) seconds;
+	
+    for(i = 0; i < 9; i++) {
+         if(smileyPos[i] == 1) { //occupied Box with a living Smiley
+             availableBoxes[size] = i;
+             size++;
+         }
+    }
+
+    if(size == 0) {
+        return 0; 
+    }
+
+    //Randomly pick an index from 0 to size - 1. Remove the smiley there
+    int randomIndex = rand();
+	randomIndex = randomIndex % size;
+	randomIndex = availableBoxes[randomIndex];
+		
+    x = randomIndex % 3;
+    y = randomIndex/3;
+    x = (215 * x) + 215/2; //approximate the center
+    y = (160 * y) + 160/2;
+
+    //Remove the smiley
+    drawSmiley(x,y,75,1,1);
+	//Mark box as empty
+    smileyPos[randomIndex] = 0;
+  
+    return 1;
+
+}
+
+/* Draws Random smileys in different grid positions that aren't
+   currently occupied. smileyPos represents positions of Smileys.
+   A value of 0 for a given index means that box is empty, a value
+   of 1 for a given index means that there is a smiley in the box,
+   and a value of -1 for a given index means that there is a dead
+   smiley in that box.
+*/
+
+int drawRandomSmileys(int numSmileys, int smileyPos[9]) {
+    int i = 0;
+    int j = 0;
+    int availableBoxes[9];
+    int size = 0;
+    int x = 0;
+    int y = 0;
+    int num_drawn = 0;
+	int randomIndex = 0;
+  //  unsigned int seed = 0;  // = clock();
+	//time_t seconds;
+	//time(&seconds);
+	//unsigned int seed = (unsigned int) seconds;
+	
+    
+    for(j = 0; j < numSmileys; j++) {
+
+        size = 0;
+        for(i = 0; i < 9; i++) {
+            if(!smileyPos[i]) { //empty Box
+                availableBoxes[size] = i;
+                size++;
+            }
+        }
+
+        if(size == 0) {
+            return 0; //We can't possibly draw anything because the grid is full
+        }
+
+        //Randomly pick an index from 0 to size - 1. Draw smiley there
+
+        // generate a random number in range from 0 to size - 1 to pick a random smiley
+		// from the array availableBoxes
+        randomIndex = rand();
+		randomIndex = randomIndex % size;
+		randomIndex = availableBoxes[randomIndex];
+		
+        x = randomIndex % 3;
+        y = randomIndex/3;
+        x = (215 * x) + (215/2); //approximate the center
+        y = (160 * y) + (160/2);
+
+        //Draw the smiley
+        drawSmiley(x,y,75,1,0);
+        smileyPos[randomIndex] = 1; //We've got a new smiley!
+        num_drawn++;
+    } 
+    return num_drawn;
+
+}
+
+
+/* Draw a 3 x 3 grid onto the screen. The dimensions are as follows:
+   Box(0,0) = 0,0 to 214,159
+   Box(1,0) = 216,0 to 430,159
+   Box(2,0) = 432,0 to 639,159
+   Box(1,0) = 0,161 to 214,320
+   Box(1,1) = 216,161 to 430,320
+   Box(1,2) = 432,161 to 639,320
+   Box(2,0) = 0,322 to 214,479
+   Box(2,1) = 216,322 to 430,479
+   Box(2,2) = 432,322 to  639,479 
+*/
+
+void drawGrid(COLOR c) {
+    unsigned int* pDisplay_data   = (unsigned int  *)0x07E00000;
+    int i = 0;
+    int x_current = 0; 
+    int y_current = 0;
+
+    //Draw horizontal lines
+    for(i = 0; i < 640; i++) {
+        colorPixel(c, i , 160, &pDisplay_data, &x_current, &y_current);
+    }
+    //Draw horizontal lines
+    for(i = 0; i < 640; i++) {
+        colorPixel(c,i,321,&pDisplay_data, &x_current, &y_current);
+    }
+
+    //Draw vertical lines
+    for(i = 0; i < 480; i++) {
+        colorPixel(c,215,i,&pDisplay_data, &x_current, &y_current);
+    }
+
+    //Draw vertical lines
+    for(i = 0; i < 480; i++) {
+        colorPixel(c,431,i,&pDisplay_data, &x_current, &y_current);
+    }
+}
+    
+
+/* Change the value of GridX,GridY to specify the box the arguments x,y lie in.
+   GridX,GridY take values from 0,0 to 2,2 and represent the boxes in the grid
+   treating the top-left most box as box 0,0 and the bottom right box
+   as box 2,2. Set GridX and GridY to -1,-1 if x,y falls in no grid.
+*/
+void calculateGridPosition(int* GridX, int* GridY, int x, int y) {
+    if(x >= 0 && x < 215) { //Lies in first column
+        *GridX = 0;
+        if( y >= 0 && y < 160)
+            *GridY = 0;
+        else if( y > 160 && y < 321 )
+            *GridY = 1;
+        else if( y > 321 && y < 480 )
+            *GridY = 2;
+        else {
+            *GridX = -1;
+            *GridY = -1;
+        }
+        return; 
+    }
+    if(x > 215 && x < 431) { //Lies in second column
+        *GridX = 0;
+        if( y >= 0 && y < 160)
+            *GridY = 0;
+        else if( y > 160 && y < 321 )
+            *GridY = 1;
+        else if( y > 321 && y < 480 )
+            *GridY = 2;
+        else {
+            *GridX = -1;
+            *GridY = -1;
+        }
+        return;
+    }
+    if(x > 431 && x < 640) { //Lies in third column
+        *GridX = 0;
+        if( y >= 0 && y < 160)
+            *GridY = 0;
+        else if( y > 160 && y < 321 )
+            *GridY = 1;
+        else if( y > 321 && y < 480 )
+            *GridY = 2;
+        else {
+            *GridX = -1;
+            *GridY = -1;
+        }
+        return;
+    }
+   
+    *GridX = -1;
+    *GridY = -1;
+}
+
+
+void testHandPlacement() {
+    while(1) { 
+        //Read coordinates from GPIO
+        //displayHandPosition(x,y,0);
+    }
+}
+
+
+
+/* Takes a coordinate x,y and draws a small circle to represent
+   the position of one of the user's hands. isOpen specifies 
+   whether the user's hands have opened (indicating a hit)
+*/
+
+
+void displayHandPosition(int x, int y, int isOpen) {
+    //Ignore isOpen for now
+    drawCircle(x,y,10,magenta);
+}
+
 
 
 /* 
@@ -160,17 +460,24 @@ unsigned int* drawX(int x, int y, int pixels_in_branch_of_x, COLOR c) {
    whether the face is dead or alive (0 = dead, 1 = alive).
    If the face is dead, draw a frowning face with X'ed out
    eyes. If the face has not been hit, draw a smiling
-   face with circles for eyes.
+   face with circles for eyes. Use is_removing_a_smiley to
+   specify whether you intend to remove a smiley.
 */
 
 
-void drawSmiley(int x, int y, int radius, int is_alive) {
+void drawSmiley(int x, int y, int radius, int is_alive, int is_removing_a_smiley) {
     int dist_mouth_from_center = radius*.6;
 	int dist_eyes_from_center = radius/2;
 	int pixels_in_branch_of_x = 8; //How many pixels in each of the 4 branches that compose the letter "X"
 	COLOR border_color = yellow;
         COLOR mouth_color  = magenta;
-	COLOR eye_color = blue;
+	COLOR eye_color = cyan;
+
+        if(is_removing_a_smiley) {
+            border_color = black;
+            mouth_color = black;
+            eye_color = black;
+        }
 
     //Draw the smiley's border
     drawCircle(x,y,radius,border_color);
@@ -179,26 +486,26 @@ void drawSmiley(int x, int y, int radius, int is_alive) {
 	if(is_alive) {
 	    //Draw the right eye first. .707 is approximately cos(45)= sin(45)
 	    drawCircle(x + (int)(dist_eyes_from_center * .707) , 
-		      y - (int)(dist_eyes_from_center * .707), 10, cyan);
+		      y - (int)(dist_eyes_from_center * .707), 10, eye_color);
 	    //Draw the left eye next. 
 		drawCircle(x - (int)(dist_eyes_from_center * .707) , 
-		      y - (int)(dist_eyes_from_center * .707), 10, cyan);	
+		      y - (int)(dist_eyes_from_center * .707), 10, eye_color);	
 			  
 	    //Now draw an arc to represent a smiley face
-		drawArc(x, y + dist_mouth_from_center, 20, magenta, 0);	
+		drawArc(x, y + dist_mouth_from_center, 20, mouth_color, 0);	
 	}
 	else {
 	    //Draw an X in place of each eye
 		
 	    //Draw the right eye first. .707 is approximately cos(45)= sin(45)
 		drawX(x + (int)(dist_eyes_from_center * .707) , 
-		      y - (int)(dist_eyes_from_center * .707), 10, cyan);
+		      y - (int)(dist_eyes_from_center * .707), 10, eye_color);
 	    //Draw the left eye next.
 		drawX(x - (int)(dist_eyes_from_center * .707) , 
-		      y - (int)(dist_eyes_from_center * .707), 10, cyan);	
+		      y - (int)(dist_eyes_from_center * .707), 10, eye_color);	
 			  
 	    //Now draw an upside down arc to represent a frowning face
-	    drawArc(x, y + dist_mouth_from_center, 20, magenta, 1);	
+	    drawArc(x, y + dist_mouth_from_center, 20, mouth_color, 1);	
 	}
 	
 }
